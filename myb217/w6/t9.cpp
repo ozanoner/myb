4a6226ac23a43e513679673ebb5e77069bce5852
Vedat Ozan Öner <ozan.oner@gmail.com>
2017-11-02 09:00:10 +0300

    updated

--- myb217/w6/t9.cpp
+++ myb217/w6/t9.cpp
@@ -16,12 +16,15 @@ enum class SiraTipi {
 	Bireysel
 };
 
+// Sira olusturulduktan sonra degistirilemez.
 class Sira {
 public:
 	Sira(SiraTipi tip, int sira_no): _tip(tip), _sira_no(sira_no) { }
+// banka gorevlisi kullanimi icin
 	bool kontrol(SiraTipi t, int s) const {
 		return s==this->_sira_no && t==this->_tip;
 	}
+// guncellenmesi gerekiyor
 	string str() {
 		return "";
 	}
@@ -30,7 +33,9 @@ private:
 	SiraTipi _tip;
 };
 
-
+// banka musterisi
+// gise veya musteri temsilcisinden sira alir
+// soruldugunda aldigi sirayi soyler
 class Musteri {
 public:
 	Musteri(int no, const string& adi, const string& soyadi):
@@ -43,6 +48,7 @@ public:
 		this->_sira = &s;
 	}
 	const Sira& sira_soyle() const {
+		cout << ">> Ben " << this->_adi << ". islem yapmak istiyorum" << endl;
 		return *this->_sira;
 	}
 
@@ -52,39 +58,53 @@ private:
 	const Sira* _sira; 
 };
 
-
+// banka islemleri
+// 1- musteriye sira verir 
+// 2- gise islemi veya bireysel islem yapar
 class Banka {
 public:
 	Banka(): _gise_sirano(0), _bireysel_sirano(0),\
 		_gise_sira_onu(1), _bireysel_sira_onu(1) { }
 
+	// yeni sira numarasi olusturup gelen musteriye verir
+	// musteriyi de siraya ekler
 	void gise_sira_ver(Musteri* musteri) {
 		Sira* s= new Sira(SiraTipi::Gise, ++this->_gise_sirano);
 		this->_siralar.insert(s);
 		musteri->sira_al(*s);
 		this->_gise.push_back(musteri);
+		cout << "[bilgi]: yeni sira no verildi (" << this->_gise_sirano << ")\n";
 	}
 
+	// gelen musterinin sirasini kontrol edip
+	// sira ondaysa islem yapar
 	bool gise_islem(const Sira& sira) {
+		// belirtilen sira numarasi, siranin basinda mi?
 		if(sira.kontrol(SiraTipi::Gise, this->_gise_sira_onu)) {
+			// musteri kabul edildi
 			Musteri* m = this->_gise.front();
-			cout << "Gise Islem yapiliyor: " << m->str() << endl;
+			cout << ">> Hosgeldiniz\n";
+			cout << "[bilgi]: Gise Islem yapiliyor: " << m->str() << endl;
+			// islem yapildi ve siradan cikarildi
 			this->_gise.pop_front();
-			cout << "Gise Islem tamamlandi\n";
+			cout << "[bilgi]: Gise Islem tamamlandi\n";
 
+			// sira onu bir sonraki numarada
 			this->_gise_sira_onu++;
+
+			// sira bileti listeden cikarildi
 			auto it = this->_siralar.find(&sira);
 			if(it!=this->_siralar.end()) {
 				delete *it;
 				this->_siralar.erase(it);
 			}
 			else 
-				cout << "sira bulunamadi\n";
+				cout << "[hata]: sira bulunamadi\n";
 
 			return true;
 		}
 		else {
-			cout << "hata: siranizi bekleyiniz..\n";
+			cout << ">> Siranizi bekleyiniz lutfen..\n";
 			return false;
 		}
 	}
@@ -94,14 +114,18 @@ public:
 	bool bireysel_islem(const Sira& sira) {
 	}
 private:
+	// giseden verilen en son sira-no
 	int _gise_sirano;
+	// siranin en basindaki numara
 	int _gise_sira_onu;
+	// musteri sirasi
 	deque<Musteri*> _gise;
 
 	int _bireysel_sirano;
 	int _bireysel_sira_onu;
 	deque<Musteri*> _bireysel;
 
+	// verilen siralarin tutuldugu set
 	set<const Sira*> _siralar;
 };
 
@@ -111,11 +135,22 @@ int main() {
 	Musteri* ali = new Musteri(234, "ali", "yanmaz");
 	Musteri* ayse = new Musteri(111, "ayse", "sonmez");
 
+	// once ali sonra ayse sira aldi
 	banka.gise_sira_ver(ali);
 	banka.gise_sira_ver(ayse);
 
 
+	// ayse once islem yapamaz, sirasini beklemeli
 	banka.gise_islem(ayse->sira_soyle());
+
 	banka.gise_islem(ali->sira_soyle());
 	banka.gise_islem(ayse->sira_soyle());
+
+
+	// gorev: sira biletlerini bastirma
+	// gorev: bireysel islemlerin kodlanmasi
+	// gorev: bireysel islemlerin sirasina musteri ekleme ve islemler
 }
+
+
+
